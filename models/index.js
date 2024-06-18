88d09f1f4a293ed784923ec63d06dd944f68f195
Carlos Jorge Gonzalez Perales <carlosgzzperales@gmail.com>
2024-06-18 16:42:05 -0600

    Added the associations of product_id to Product and Category models

--- models/index.js
+++ models/index.js
@@ -5,9 +5,13 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+});
 // Products belongToMany Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
