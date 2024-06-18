a6ed566c523b3d8edd2e1c8bed432fab7efdd981
Carlos Jorge Gonzalez Perales <carlosgzzperales@gmail.com>
2024-06-18 17:20:22 -0600

    Added route to get all categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,7 +3,13 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
   // find all categories
   // be sure to include its associated Products
 });
