6549fbf409c348ade80609d3e5d6d2635b67a1e0
Carlos Jorge Gonzalez Perales <carlosgzzperales@gmail.com>
2024-06-19 16:55:54 -0600

    added route to create a new tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -33,9 +33,15 @@ router.get('/:id', async (req, res) => {
     res.status(500).json(err)
   }
 });
+// create a new tag
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
