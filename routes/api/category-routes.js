2f9433ffb3097fe2a426ff4d7a2e39514eb9e1ad
Carlos Jorge Gonzalez Perales <carlosgzzperales@gmail.com>
2024-06-18 18:02:03 -0600

    Added the update route to category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,8 +44,22 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+// update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedCategory = await Category.update({
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id
+      },
+    }
+    );
+    res.status(200).json(updatedCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
