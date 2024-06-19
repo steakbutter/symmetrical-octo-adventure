f41d131eba5c6ad1c899e23b23b197e44f4fcf15
Carlos Jorge Gonzalez Perales <carlosgzzperales@gmail.com>
2024-06-19 16:52:42 -0600

    created a route to get one tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -15,10 +15,23 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
+// find a single tag by its `id`
+// be sure to include its associated Product data
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!tagData) {
+      res.status(400).json({ message: 'No tag found with that id.' });
+      return;
+    }
+    res.status(200).json(tagData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.post('/', (req, res) => {
